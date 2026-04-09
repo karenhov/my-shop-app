d88856c0743328aebc75bbb785868e06fcf97b0e
kraklin <40805745+karenhov@users.noreply.github.com>
2026-04-10 02:09:11 +0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -595,9 +595,9 @@ async function startServer() {
     });
   }
 
-  const PORT = 3000;
+  const PORT = parseInt(process.env.PORT || "3000");
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 }
 
